f841bce57c10288ba14407f588197fe348770a9b
Namratawaje20 <wajenamrata123@gmail.com>
2024-12-27 22:07:08 +0530

    added images

--- script.js
+++ script.js
@@ -1,48 +1,50 @@
 const participants = [
-    { name: 'Namrata', image: 'w1.png' },
-      { name: 'Pratik', image: 'b1.png' },
-      { name: 'AAditya', image: 'b2.png' },
-      { name: 'Sarthak', image: 'b3.png' },
-      { name: 'Vishakha', image: 'w2.png' },
-      { name: 'Rushi', image: 'b1.png' },
-      { name: 'Darshan', image: 'b2.png' },
-      { name: 'Geeta', image: 'w3.png' },
-      { name: 'Pallavi', image: 'w1.png' },
-      { name: 'tejas', image: 'b1.png' },
-      { name: 'Prashant', image: 'b2.png' },
-      { name: 'Suvarna', image: 'w1.png' },
-      { name: 'Varsha', image: 'w2.png' },
-      { name: 'Akshada', image: 'w3.png' },
-      { name: 'Tanuja', image: 'w1.png' },
-      { name: 'Sumit', image: 'b3.png' },
-      { name: 'Deven', image: 'w3.png'},
-      { name: 'Tushar', image: 'b1.png' },
-      { name: 'sudarshan', image: 'b2.png' },
-      { name: 'santu', image: 'b3.png' },
-      { name: 'Megha', image: 'w2.png' },
-    ];
-    
-    const participantsContainer = document.getElementById('participants-container');
-    
-    function displayParticipants(participantList) {
-      participantsContainer.innerHTML = '';
-      participantList.forEach((participant) => {
-        participantsContainer.innerHTML += `
-          <div class='participant-card'>
-            <img src="${participant.image}" alt="${participant.name}">
-            <span>${participant.name}</span>
-          </div>
-        `;
-      });
-    }
-    
-    displayParticipants(participants);
-    
-    function filterParticipants() {
-      const searchQuery = document.getElementById('search').value.toLowerCase();
-      const filteredParticipants = participants.filter(participant => 
-        participant.name.toLowerCase().includes(searchQuery)
-      );
-      displayParticipants(filteredParticipants);
-    }
-    
+  { name: 'Namrata', image: './img/woman-1.png' },
+  { name: 'Pratik', image: './img/boy-3.png' },
+  { name: 'Aaditya', image: './img/boy-2.png' },
+  { name: 'Tejas', image: './img/boy-1.png' },
+  { name: 'Geeta', image: './img/woman-2.jpg' },
+  { name: 'Tushar', image: './img/boy-1.png' },
+  { name: 'Sarthak', image: './img/boy-2.png' },
+  { name: 'Pallavi', image: './img/woman-1.png' },
+  { name: 'Suvarna', image: './img/woman-3.jpg' },
+  { name: 'Sudarshan', image: './img/boy-2.png' },
+  { name: 'Svarup', image: './img/boy-3.png' },
+  { name: 'Rutuja', image: './img/woman-1.png' },
+  { name: 'Rohini', image: './img/woman-2.jpg' },
+  { name: 'Bharti', image: './img/woman-3.jpg' },
+  { name: 'Seema', image: './img/woman-2.jpg' },
+  { name: 'Aaryan', image: './img/boy-2.png' },
+  { name: 'Patu', image: './img/boy-1.png' },
+  { name: 'Shevta', image: './img/woman-1.png' },
+  { name: 'Netra', image: './img/woman-2.jpg' },
+  { name: 'Vishakha', image: './img/woman-2.jpg' },
+  { name: 'Tanuja', image: './img/woman-1.png' },
+  { name: 'Chetan', image: './img/boy-1.png' },
+  { name: 'Jivitesh', image: './img/boy-3.png' },
+];
+
+
+const participantsContainer = document.getElementById('participants-container');
+
+function displayParticipants(participantList) {
+  participantsContainer.innerHTML = '';
+  participantList.forEach((participant) => {
+    participantsContainer.innerHTML += `
+      <div class='participant-card'>
+        <img src="${participant.image}" alt="${participant.name}" onerror="this.src='./img/placeholder.png'">
+        <span>${participant.name}</span>
+      </div>
+    `;
+  });
+}
+
+displayParticipants(participants);
+
+function filterParticipants() {
+  const searchQuery = document.getElementById('search').value.toLowerCase();
+  const filteredParticipants = participants.filter(participant => 
+    participant.name.toLowerCase().includes(searchQuery)
+  );
+  displayParticipants(filteredParticipants);
+}
